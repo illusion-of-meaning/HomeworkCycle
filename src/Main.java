4d2d93393b07f2b30a5e763e341acfed5e0e99be
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-09-05 19:40:46 +0900

    Задания 2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        // 1
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
-        }
+        {
+            // 1
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
 
-        // 2
-        for (int i = 10; i > 0; i--) {
-            System.out.println(i);
-        }
+            // 2
+            for (int i = 10; i > 0; i--) {
+                System.out.println(i);
+            }
 
-        // 3
-        for (int i = 0; i <= 17; i++) {
-            if (i % 2 == 0) {
+            // 3
+            for (int i = 0; i <= 17; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+
+            // 4
+            for (int i = 10; i >= -10; i--) {
                 System.out.println(i);
             }
         }
+        {
+            // 1
+            for (int i = 1904; i <= 2096; i += 4) {
+                System.out.println(i + " год является високосным");
+            }
 
-        // 4
-        for (int i = 10; i >= -10; i--) {
-            System.out.println(i);
+            // 2
+            for (int i = 1; i <= 14; i++) {
+                System.out.println(7 * i);
+            }
+
+            //3
+            int tmp = 1;
+            for (int i = 1; i < 10; i++) {
+                System.out.println(tmp *= 2);
+            }
         }
     }
 }
